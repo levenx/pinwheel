d9c434da55504d54e336e61c218ad42e83ea6324
sunlejin <sunlejin@bytedance.com>
2025-09-18 20:57:55 +0800

    feat: init

---
+++ packages/pinwheel-extension-website/app/_meta.tsx
@@ -0,0 +1,19 @@
+const meta = {
+  index: {
+    title: "Basic11",
+    theme: {
+      sidebar: false,
+    },
+  },
+  "--": {
+    type: "separator",
+    title: (
+      <div className="flex items-center gap-2">
+        <div>11</div>
+      </div>
+    ),
+  },
+  docs: "Docs",
+};
+
+export default meta;
