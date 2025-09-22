6f0582623f1af945e22c854e77cc7c350e9de1f2
levenx <levenx007@gmail.com>
2025-09-22 23:29:59 +0800

    下载文档指引

--- packages/pinwheel-extension-website/app/_meta.tsx
+++ packages/pinwheel-extension-website/app/_meta.tsx
@@ -1,13 +1,13 @@
 const meta = {
   index: {
-    title: "Basic11",
+    title: "Basic",
     display: "hidden",
     theme: {
       sidebar: false,
     },
   },
   docs: "操作文档",
-  downloads: "下载",
+  downloads: "插件下载",
   blogs: "开发者博客",
 };
 
